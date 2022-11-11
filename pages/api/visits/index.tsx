f4f8edffe162e072ad46787de1303a5f6b21d1a5
Sergei Kalugin <serhii.kaluhin@nure.ua>
2022-11-11 03:58:21 +0200

    create visit

---
+++ pages/api/visits/index.tsx
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../lib/prisma'
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const visits = await prisma.visit.findMany()
+    res.json(visits)
+  } else if (req.method == 'POST' ) {
+    const visit = await prisma.visit.create({
+      data: {
+        room_id: req.body.room_id,
+        is_enter: req.body.is_enter,
+        creation: req.body.creation,
+      },
+    })
+    res.json(visit)
+  }
+}

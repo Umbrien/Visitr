29525eae33a0dc101aede6bbca8d103026b138b1
Sergei Kalugin <serhii.kaluhin@nure.ua>
2022-11-11 03:19:50 +0200

    post room

--- pages/api/rooms/index.tsx
+++ pages/api/rooms/index.tsx
@@ -9,5 +9,13 @@ export default async function handler(
   if (req.method === 'GET') {
     const rooms = await prisma.room.findMany()
     res.json(rooms)
+  } else if (req.method === 'POST') {
+    const room = await prisma.room.create({
+      data: {
+        name: req.body.name,
+        max_capacity: req.body.max_capacity,
+      },
+    })
+    res.json(room)
   }
 }
